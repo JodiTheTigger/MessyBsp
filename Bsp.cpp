c35f4f1acfb1cfde9ec8cc3d41ba9d5591023c47
Richard Maxwell <jodi.the.tigger@gmail.com>
2014-11-20 14:27:22 +1300

    Added my own BSP loader written in C++14.

---
+++ Bsp.cpp
@@ -0,0 +1,125 @@
+/*
+    MessyBsp. BSP collision and loading example code.
+    Copyright (C) 2014 Richard Maxwell <jodi.the.tigger@gmail.com>
+    This file is part of MessyBsp
+    MessyBsp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include "Bsp.hpp"
+
+#include <cstdlib>
+
+namespace Bsp {
+
+/// This is bare bones, with no nice error reporting
+/// or proper exception safe cleanup. C++ makes it too ugly.
+void GetCollisionBsp(
+        const std::string &filePath,
+        CollisionBsp &bsp)
+{
+    auto fileHandle = fopen(filePath.c_str(), "r+b");
+
+    // Check if the file exists.
+    if (!fileHandle)
+    {
+        return;
+    }
+
+    // Using do once + continue in leiu of scoped_exit
+    do
+    {
+        // Read the header.
+        if (!fread(&bsp.header, 1, sizeof(Header), fileHandle))
+        {
+            continue;
+        }
+
+        // basic checks
+        if  (
+                (bsp.header.version != cQuake3BspVersion) ||
+                (bsp.header.magicString[0] != 'I') ||
+                (bsp.header.magicString[1] != 'B') ||
+                (bsp.header.magicString[2] != 'S') ||
+                (bsp.header.magicString[3] != 'P')
+            )
+        {
+            continue;
+        }
+
+        // Count
+        const auto& lumps = bsp.header.lumps;
+        size_t Counts[Lumps::Count] =
+        {
+            0,
+            lumps[Textures].byteCount       / sizeof(Texture),
+            lumps[Planes].byteCount         / sizeof(Plane),
+            lumps[Nodes].byteCount          / sizeof(Node),
+            lumps[Leaves].byteCount         / sizeof(Leaf),
+            0,
+            lumps[LeafBrushes].byteCount    / sizeof(LeafBrush),
+            0,
+            lumps[Brushes].byteCount        / sizeof(Brush),
+            lumps[BrushSides].byteCount     / sizeof(BrushSide),
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        };
+
+        // Reserve
+        bsp.textures.reserve(Counts[Textures]);
+        bsp.planes.reserve(Counts[Planes]);
+        bsp.nodes.reserve(Counts[Nodes]);
+        bsp.leaves.reserve(Counts[Leaves]);
+        bsp.leafBrushes.reserve(Counts[LeafBrushes]);
+        bsp.brushes.reserve(Counts[Brushes]);
+        bsp.brushSides.reserve(Counts[BrushSides]);
+
+        // Read Textures
+        auto readTypes = [&] (Lumps lumpEnum, auto& vector, size_t typeSize)
+        {
+            fseek(
+                fileHandle,
+                bsp.header.lumps[lumpEnum].offsetInBytesFromStartOfFile,
+                SEEK_SET);
+
+            for (unsigned i = 0; i < Counts[lumpEnum]; ++i)
+            {
+                vector.push_back({});
+
+                if (!fread(&vector.back(), 1, typeSize, fileHandle))
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        };
+
+        if (!readTypes(Textures,    bsp.textures,       sizeof(Brush)))     continue;
+        if (!readTypes(Planes,      bsp.planes,         sizeof(Plane)))     continue;
+        if (!readTypes(Nodes,       bsp.nodes,          sizeof(Node)))      continue;
+        if (!readTypes(Leaves,      bsp.leaves,         sizeof(Leaf)))      continue;
+        if (!readTypes(LeafBrushes, bsp.leafBrushes,    sizeof(LeafBrush))) continue;
+        if (!readTypes(Brushes,     bsp.brushes,        sizeof(Brush)))     continue;
+        if (!readTypes(BrushSides,  bsp.brushSides,     sizeof(BrushSide))) continue;
+
+    } while(false);
+
+    fclose(fileHandle);
+}
+
+} // namespace
---
+++ Bsp.hpp
@@ -0,0 +1,144 @@
+/*
+    MessyBsp. BSP collision and loading example code.
+    Copyright (C) 2014 Richard Maxwell <jodi.the.tigger@gmail.com>
+    This file is part of MessyBsp
+    MessyBsp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+// Information taken from:
+// http://www.mralligator.com/q3/
+// http://www.flipcode.com/archives/Simple_Quake3_BSP_Loader.shtml
+
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include <string>
+
+namespace Bsp {
+
+enum Lumps
+{
+    Entities = 0,
+    Textures,
+    Planes,
+    Nodes,
+    Leaves,
+    LeafFaces,
+    LeafBrushes,
+    Models,
+    Brushes,
+    BrushSides,
+    Vertexes,
+    Meshverts,
+    Effects,
+    Faces,
+    Lightmaps,
+    Lightvols,
+    Visdata,
+
+    Count,
+};
+
+const unsigned cQuake3BspVersion = 0x2e;
+
+struct Lump
+{
+    int32_t offsetInBytesFromStartOfFile;
+
+    /// Always a multiple of 4.
+    int32_t byteCount;
+};
+
+struct Header
+{
+    // Always "IBSP"
+    char magicString[4];
+
+    // 0x2e for the BSP files distributed with Quake 3.
+    int32_t version;
+    Lump lumps[Lumps::Count];
+};
+
+struct Texture
+{
+    char    name[64];
+    int32_t surfaceFlags;
+    int32_t contentFlags;
+};
+
+/// Note that planes are paired. The pair of planes with indices i and i ^ 1
+/// are coincident planes with opposing normals.
+struct alignas(16) Plane
+{
+    float normal[3];
+
+    /// Distance from origin to plane along normal.
+    float distance;
+};
+
+struct Node
+{
+    int32_t planeIndex;
+
+    /// Children indices. Negative numbers are leaf indices: -(leaf+1).
+    int32_t childIndex[2];
+
+    int32_t boundsMin[3];
+    int32_t boundsMax[3];
+};
+
+struct Leaf
+{
+    int32_t visdataClusterIndex;
+    int32_t areaPortal;
+    int32_t boundsMin[3];
+    int32_t boundsMax[3];
+    int32_t firstLeafFaceIndex;
+    int32_t leafFaceCount;
+    int32_t firstLeafBrushIndex;
+    int32_t leafBrushCount;
+};
+
+struct LeafBrush
+{
+    int32_t brushIndex;
+};
+
+struct Brush
+{
+    int32_t firstBrushSideIndex;
+    int32_t sideCount;
+    int32_t textureIndex;
+};
+
+struct BrushSide
+{
+    int32_t planeIndex;
+    int32_t textureIndex;
+};
+
+struct CollisionBsp
+{
+    Header                  header;
+    std::vector<Texture>    textures;
+    std::vector<Plane>      planes;
+    std::vector<Node>       nodes;
+    std::vector<Leaf>       leaves;
+    std::vector<LeafBrush>  leafBrushes;
+    std::vector<Brush>      brushes;
+    std::vector<BrushSide>  brushSides;
+};
+
+void GetCollisionBsp(const std::string& filePath, CollisionBsp& bsp);
+
+} // namespace
