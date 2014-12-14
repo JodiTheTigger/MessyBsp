4c494f80cc2f94e8a4b8f07e6b300dae6c712618
Richard Maxwell <jodi.the.tigger@gmail.com>
2014-12-15 06:29:30 +1300

    Added SDL event loop, can now quit the app.

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@
 // Need ifdef for different platforms.
 #include <GL/glew.h>
 
+void DoGraphics();
+
 int main(int, char**)
 {
     Bsp::CollisionBsp bsp;
@@ -36,51 +38,78 @@ int main(int, char**)
 
     printf("Trace Took %ld microseconds\n", result.count());
 
-    // RAM: Lets try loaind sdl and get a glcontext.
+    // RAM: sdl testing.
+    DoGraphics();
+
+    return 0;
+};
+
+
+// RAM: Lets try loaind sdl and get a glcontext.
+void DoGraphics()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Window mode MUST include SDL_WINDOW_OPENGL for use with OpenGL.
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2/OpenGL Demo", 0, 0, 640, 480,
+        SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+
+    // Create an OpenGL context associated with the window.
+    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+    SDL_GL_MakeCurrent(window, glcontext);
+
+    // Now I can init glew.
     {
-        SDL_Init(SDL_INIT_VIDEO);
+        glewExperimental = GL_TRUE;
 
-        // Window mode MUST include SDL_WINDOW_OPENGL for use with OpenGL.
-        SDL_Window *window = SDL_CreateWindow(
-            "SDL2/OpenGL Demo", 0, 0, 640, 480,
-            SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
-
-        // Create an OpenGL context associated with the window.
-        SDL_GLContext glcontext = SDL_GL_CreateContext(window);
-
-        SDL_GL_MakeCurrent(window, glcontext);
-
-        // Now I can init glew.
+        GLenum err = glewInit();
+        if (GLEW_OK != err)
         {
-            glewExperimental = GL_TRUE;
+            /* Problem: glewInit failed, something is seriously wrong. */
+            fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
 
-            GLenum err = glewInit();
-            if (GLEW_OK != err)
+        }
+
+        fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    }
+
+    // MAIN SDL LOOP
+    bool running = true;
+    while (running)
+    {
+        SDL_Event e;
+
+        if (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
             {
-                /* Problem: glewInit failed, something is seriously wrong. */
-                fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-
+                running = false;
             }
-            fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
+            if (e.type == SDL_KEYUP)
+            {
+                if (e.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    running = false;
+                }
+            }
         }
 
         // now you can make GL calls.
         glClearColor(0,1,0,1);
         glClear(GL_COLOR_BUFFER_BIT);
         SDL_GL_SwapWindow(window);
-
-        // Once finished with OpenGL functions, the SDL_GLContext can be deleted.
-        SDL_GL_DeleteContext(glcontext);
-
-        SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-
-        // Close and destroy the window
-        SDL_DestroyWindow(window);
-
-        // Clean up
-        SDL_Quit();
     }
 
-	
-	return 0;
-};
+
+    // Once finished with OpenGL functions, the SDL_GLContext can be deleted.
+    SDL_GL_DeleteContext(glcontext);
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+}
